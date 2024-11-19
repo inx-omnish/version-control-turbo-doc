4a72687ffafac28b4353488aa7f9d0cb3bb4019e
Omnish <omnish007@gmail.com>
2024-11-19 17:19:42 +0530

    More new changes

--- app/page.js
+++ app/page.js
@@ -4,6 +4,9 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
+      <h1>More New changes</h1>
+      <h1>New changes</h1>
+      <h1>New changes</h1>
     </div>
   );
 }
